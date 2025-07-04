bb9cce721ecbb97aefddbce562e1db3df6066f11
Ndoping Wilson <w770106@gmail.com>
2025-07-04 14:02:27 +0100

    this is the second commit of jul 4

--- src/pages/productListPage/ProductListPage.tsx
+++ src/pages/productListPage/ProductListPage.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+
 import { Link, useLocation } from "react-router-dom";
 import type { Product, Products } from "../../components/types/types";
 import { useFetch } from "../../hooks/useFetch";
